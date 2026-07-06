bd758ffbef58e6865224d00c112835a40b76d316
Ganesh <royalrganesh28@gmail.com>
2026-07-06 20:00:10 +0530

    Initial Commit

---
+++ expenses/expensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  expensesApp.swift
+//  expenses
+//
+//  Created by Ganesh R on 06/07/26.
+//
+
+import SwiftUI
+
+@main
+struct expensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ expensesTests/expensesTests.swift
@@ -0,0 +1,16 @@
+//
+//  expensesTests.swift
+//  expensesTests
+//
+//  Created by Ganesh R on 06/07/26.
+//
+
+import Testing
+
+struct expensesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
